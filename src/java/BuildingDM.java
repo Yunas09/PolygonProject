dbaa768e9473a1cfd1e288509c33a6bbad914dea
Yuyu <Yuyu@10.20.145.223>
2016-04-01 09:50:44 +0200

    update 1

--- src/java/BuildingDM.java
+++ src/java/BuildingDM.java
@@ -1,13 +1,21 @@
+
+import com.mysql.jdbc.Connection;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Yuyu
  */
 public class BuildingDM {
+
+    Connection conn;
     
+    public BuildingDM(){
+        Connector connect = new Connector();
+       conn = (Connection) connect.getConnection();
+    }
 }
