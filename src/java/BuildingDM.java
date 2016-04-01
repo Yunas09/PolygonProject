1e1cb84c26fb138c0eeea9b9e4a14e121dd9967a
Yuyu <Yuyu@10.20.145.223>
2016-04-01 10:48:53 +0200

    update 1

--- src/java/BuildingDM.java
+++ src/java/BuildingDM.java
@@ -1,6 +1,9 @@
 
 import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -14,9 +17,43 @@ import java.sql.SQLException;
 public class BuildingDM {
 
     Connection conn;
-    
-    public BuildingDM() throws SQLException, ClassNotFoundException{
+
+    public BuildingDM() throws SQLException, ClassNotFoundException {
         Connector connect = new Connector();
-       conn = (Connection) connect.getConnection();
+        conn = (Connection) connect.getConnection();
+
+    }
+
+    public ArrayList<Building> BuildingDM(Connection con) throws SQLException {
+        ArrayList Building = new Building();
+
+        Statement stmt = null;
+        String query = "select * from Building_Name";
+
+        try {
+            stmt = (Statement) con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+
+                String Building_Name = rs.getInt("Building_Name");
+                String Building_Adress = rs.getString("Building_Adress");
+                int Building_No = rs.getInt("Building_No");
+                int Building_Size = rs.getInt("Building_Size");
+                int Building_cond = rs.getInt("Building_cond");
+                Building.add(new Building(Building_Name, Building_Adress, Building_No, Building_Size, Building_cond));
+//                System.out.println(
+//                        Player_pos + "\t" + Player_num + "\t"
+//                        + teamID + "\t" + Player_name);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+
+            }
+            System.out.println(Building);
+        }
+        return null;
     }
 }
---
+++ web/Startside.jsp
@@ -0,0 +1,21 @@
+<%-- 
+    Document   : Startside
+    Created on : 01-04-2016, 10:17:14
+    Author     : Yuyu
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <form>
+            <input type="text" name="Building_Name" value="" />   
+            
+        </form>
+    </body>
+</html>
