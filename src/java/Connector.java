503e0a80cd992b0cab0c9a608943f5e89bd82550
Yuyu <Yuyu@10.20.145.223>
2016-04-04 10:12:26 +0200

    Merge origin/master
    Conflicts:
    	src/java/BuildingDM.java

--- src/java/Connector.java
+++ src/java/Connector.java
@@ -20,38 +20,5 @@ import java.sql.Statement;
  * @author Yuyu
  */
 public class Connector {
-    private final String HOST     = "Localhost";
-    private final int    PORT     = 3306;
-    private final String DATABASE = "db1";
-    private final String USERNAME = "root"; 
-    private final String PASSWORD = "";
-    private Connection connection;
-    
-    public Connector() throws SQLException, ClassNotFoundException{
-        Class.forName("com.mysql.jdbc.Driver");
-        String url = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE;
-        connection = DriverManager.getConnection(url, USERNAME, PASSWORD);
-    private final String HOST = "Localhost";
-    private final int PORT = 3306;
-    private final String DATABASE = "polygon1";
-    private String USERNAME = "root";
-    private String PASSWORD = "Ua123459876";
-    private String URL = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE;
-    private Connection Connection;
-
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        new Connector().getConnection();
-    }
-    
-    public ResultSet doQuery(String query) throws SQLException{
-        System.out.println("*********"+query);
-        Statement stmt = connection.createStatement();
-        ResultSet res = stmt.executeQuery(query);
-        return res;
-    }
-    
-    public void doUpdate(String query) throws SQLException{
-        Statement stmt = connection.createStatement();
-        stmt.executeUpdate(query);
-    }
+   
 }
